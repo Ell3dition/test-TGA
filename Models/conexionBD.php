be5d2337096a9e129f76b1256accb73d246181a2
Ell3dition <marcosjara77@gmail.com>
2021-10-15 21:29:20 -0300

    ACTUALIZACION DE DATOS DE ACCESO A BASE DE DATOS

--- Models/conexionBD.php
+++ Models/conexionBD.php
@@ -1,31 +1,30 @@
 <?php
 
-class conexionBD {
+class conexionBD
+{
 
 
-   static public function cBD()
-   {
-       //BASE DE DATOS DE PRUEBA
-       $dbname = "TEST_TGA";
-       $user = 'root';
-       $pass = '';
-       $host = 'localhost';
+    static public function cBD()
+    {
+
+        $dbname = "cse68582_TEST-TGA";
+        $user = 'cse68582_test-tga';
+        $pass = 'cJNqTwuYMRAZ';
+        $host = 'localhost';
 
 
-     
-
-       try {
-           $bd = new PDO("mysql:host=$host:3306;dbname=$dbname", $user, $pass);
-           $bd->exec("set names utf8");
-           $bd->setAttribute(PDO::ATTR_EMULATE_PREPARES, FALSE);
-           $bd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-           $bd->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
-           return $bd;
-       } catch (PDOException $e) {
-
-           echo 'Fallo la conexión ' . $e->getMessage();
-       }
-   }
 
 
+        try {
+            $bd = new PDO("mysql:host=$host:3306;dbname=$dbname", $user, $pass);
+            $bd->exec("set names utf8");
+            $bd->setAttribute(PDO::ATTR_EMULATE_PREPARES, FALSE);
+            $bd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $bd->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+            return $bd;
+        } catch (PDOException $e) {
+
+            echo 'Fallo la conexión ' . $e->getMessage();
+        }
+    }
 }
